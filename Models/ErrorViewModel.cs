0d15d713704d0a26d4ffa417080c8e30a5db57c2
BaOai.Work <oaipbhe150516@fpt.edu.vn>
2024-01-23 10:28:39 +0700

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testwebc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
